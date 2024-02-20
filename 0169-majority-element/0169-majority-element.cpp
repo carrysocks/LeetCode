18700146e2b789c1622a9f56647f9d5575955095
sean <39540655+carrysocks@users.noreply.github.com>
2024-02-20 11:08:24 +0900

    Time: 23 ms (7.22%), Space: 26.8 MB (11.06%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int, int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]] += 1;
+        }
+        
+        int mx = -1;
+        int ans = 0;
+        for(auto p : m){
+            if(mx < p.second){
+                ans = p.first;
+                mx = p.second;
+            }
+        }
+        
+        return ans;
+    }
+};
