a162f5a8ddad37bc623e572bdd428ab38a2a71e4
sean <39540655+carrysocks@users.noreply.github.com>
2024-02-23 22:47:05 +0900

    Time: 5 ms (89.95%), Space: 9.3 MB (87.15%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int ms = s.size(), n = t.size(), count = 0;
+        int start = 0, end = 0, counter = t.size(), min_start = 0, min_len = INT_MAX;
         
-        unordered_map<char, int> m;
-        unordered_map<char, int> tmp_m;
+        vector<int> m(128, 0);
         
-        for(auto c : t){
-            m[c] += 1;
-            count++;
-        }
+        for(auto c : t) m[c]++;
         
-        if(s == t) return s;
-        
-        int l = 0, r = 0, cnt = 0;
-        int mn = 1e9, ml = 0, mr = 0;
-        while(r < ms){
-            char c = s[r];
-            if(m.find(c) != m.end()){
-                
-                if(tmp_m[c] + 1 <= m[c]) cnt++;
-                tmp_m[c] += 1;
-                
-                while((m.find(s[l]) == m.end()) || (tmp_m[s[l]] > m[s[l]])){
-                    if(r == 0) break;
-                    if(m.find(s[l]) != m.end()) tmp_m[s[l]] -= 1;
-                    l++;
-                }
-                
-                if(cnt >= count && mn > (r - l + 1)){
-                    mn = r - l + 1;
-                    ml = l;
-                    mr = r;
-                }
-            }
-            r++;
+        while(end < s.size()){
+            if(m[s[end]] > 0) counter--;
+            m[s[end]]--;
+            end++;
             
+            while(counter == 0){
+                if(end - start < min_len){
+                    min_start = start;
+                    min_len = end - start;
+                }
+                
+                m[s[start]]++;
+                if(m[s[start]] > 0) counter++;
+                start++;
+            }
         }
         
-        if(mn == 1e9) return "";
-        return s.substr(ml, mn);
+        return min_len == INT_MAX ? "" : s.substr(min_start, min_len);
     }
 };
