dc254639ae414f6e64f2086bfc0658b8e31f7f73
sean <39540655+carrysocks@users.noreply.github.com>
2024-02-25 15:20:16 +0900

    Time: 601 ms (81.52%), Space: 0B (100%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select a.visited_on, sum(b.amount_sum) as amount,
+round(avg(b.amount_sum), 2) as average_amount
+from 
+(select visited_on, sum(amount) from customer group by visited_on) a,
+(select visited_on, sum(amount) as amount_sum from customer group by visited_on) b
+where datediff(a.visited_on, b.visited_on) between 0 and 6
+group by a.visited_on
+having count(b.visited_on) >= 7
