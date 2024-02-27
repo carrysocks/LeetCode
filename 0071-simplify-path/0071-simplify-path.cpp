1c3cc72fcba3955274ec4223a1643587983dbb64
sean <39540655+carrysocks@users.noreply.github.com>
2024-02-28 00:24:22 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -2,33 +2,29 @@ class Solution {
 public:
     string simplifyPath(string path) {
         stack<string> s;
-        bool isSlash = false;
-        string temp = "";
+        string temp = "", ans = "";
         for(int i=0;i<path.size();i++){
             char c = path[i];
-            if(c == '/'){
-                if(isSlash && temp != "" && temp != "."){
-                    if(temp == ".." && !s.empty()) s.pop();  
-                    else if(temp != "..") s.push(temp);
-                }
-                isSlash = true;
-                temp = "";
+            if(c == '/') continue;
+
+            while(i < path.size() && path[i] != '/'){
+                temp += path[i++];
             }
-            else temp += c;
+
+            if(temp == ".") continue;
+            else if(temp == ".."){
+                if(!s.empty()) s.pop();
+            }
+            else s.push(temp);
+            temp = "";
         }
 
-        if(temp != "" && temp != "."){
-            if(temp == ".." && !s.empty()) s.pop();
-            else if(temp != "..") s.push(temp);
-        }
-
-        string ans = "";
         while(!s.empty()){
             ans = "/" + s.top() + ans;
             s.pop();
         }
 
-        if(ans == "") return "/";
+        if(ans.size() == 0) return "/";
         return ans;
     }
 };
